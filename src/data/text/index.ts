87b18e55f9d939e6dfee6c57b1069352ca9eafcf
Prunus Nira <prunusnira@gmail.com>
2022-11-23 23:30:15 +0900

    멀티랭귀지 작업 - 메인페이지 데이터 추가

---
+++ src/data/text/index.ts
@@ -0,0 +1,31 @@
+const TxtMain = {
+  ko: {
+    desc: "코드로 많은 사람들에게 다가가고 싶은 개발자입니다",
+    btn: {
+      about: "Nira를 소개합니다",
+      tech: "사용 가능한 기술 스택",
+      proj: "진행 프로젝트 목록",
+      link: "관련 링크",
+    },
+  },
+  en: {
+    desc: "A developer who wants to approach to many people",
+    btn: {
+      about: "Introduce myself",
+      tech: "Usable technical stack",
+      proj: "Experienced projects",
+      link: "Related link",
+    },
+  },
+  jp: {
+    desc: "코드로 많은 사람들에게 다가가고 싶은 개발자입니다",
+    btn: {
+      about: "Nira를 소개합니다",
+      tech: "사용 가능한 기술 스택",
+      proj: "진행 프로젝트 목록",
+      link: "관련 링크",
+    },
+  },
+};
+
+export default TxtMain;
