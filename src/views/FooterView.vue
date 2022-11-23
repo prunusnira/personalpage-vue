c550d66edb28fc4c2aa793b3bb4d78a8934303b9
Prunus Nira <prunusnira@gmail.com>
2022-11-24 00:18:19 +0900

    footer 추가

---
+++ src/views/FooterView.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      footer: "footer",
+    };
+  },
+};
+</script>
+
+<template>
+  <section :class="footer">(c) 2023 Nira / Made with vue 3 + pinia</section>
+</template>
+
+<style scoped>
+.footer {
+  width: 100%;
+  box-sizing: border-box;
+  text-align: right;
+  background-color: rgba(0, 1, 48, 0.8);
+  color: white;
+  position: fixed;
+  bottom: 0;
+  padding: 10px 20px;
+}
+</style>
