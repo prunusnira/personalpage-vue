dba0ac6fe320992bdecff903ef9756687119d2d0
Prunus Nira <prunusnira@gmail.com>
2022-11-23 13:42:16 +0900

    link 구조 추가

---
+++ src/components/link/LinkItem.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+export default {
+  props: {
+    title: String,
+    src: String,
+  },
+  data() {
+    return {
+      linkWrap: "linkWrap",
+      linkTitle: "linkTitle",
+      linkSrc: "linkSrc",
+    };
+  },
+};
+</script>
+
+<template>
+  <div :class="linkWrap">
+    <div :class="linkTitle">
+      {{ $props.title }}
+    </div>
+    <div :class="linkSrc">
+      {{ $props.src }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.linkWrap {
+  display: flex;
+  flex-direction: row;
+}
+
+.linkTitle {
+}
+
+.linkSrc {
+}
+</style>
--- src/views/LinkView.vue
+++ src/views/LinkView.vue
@@ -1,1 +1,35 @@
-<template>LINK</template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      link: "link",
+      title: "title",
+    };
+  },
+};
+</script>
+
+<template>
+  <div :class="link">
+    <div :class="title">
+      <font-awesome-icon icon="fa-solid fa-angle-double-right" />
+      Link
+      <font-awesome-icon icon="fa-solid fa-angle-double-left" />
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.link {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-size: 36px;
+}
+</style>
